433fe2a5fcc34203b2616774c883ba71d2729c35
Grimidk <thegrimidk@gmail.com>
2024-11-10 01:00:49 -0400

    clases

--- ProjectSagaWars/src/projectClasses/Fighter.java
+++ ProjectSagaWars/src/projectClasses/Fighter.java
@@ -9,5 +9,125 @@ package projectClasses;
  * @author juanmendezl
  */
 public class Fighter {
+    private int id;
+    private String saga;
+    private String name;
+    private int waitCounter;
+    private boolean healthStat;
+    private boolean attackStat;
+    private boolean speedStat;
+    private boolean skillStat;
+    private int qualityCounter;
+    private int quality;
+
+    public Fighter(int id, String saga, String name) {
+        this.id = id;
+        this.saga = saga;
+        this.name = name;
+        this.waitCounter = 0;
+        this.healthStat = (Math.random() * 100) >= 70;
+        this.attackStat = (Math.random() * 100) >= 50;
+        this.speedStat = (Math.random() * 100) >= 40;
+        this.skillStat = (Math.random() * 100) >= 60;
+        this.makeQuality();
+    }
+    
+    public final void makeQuality(){
+        if(this.healthStat) {this.qualityCounter += 1;}
+        if(this.attackStat) {this.qualityCounter += 1;}
+        if(this.speedStat) {this.qualityCounter += 1;}
+        if(this.skillStat) {this.qualityCounter += 1;}
+
+        if(this.qualityCounter >= 4) {this.quality = 3;}
+        else if(this.qualityCounter >= 2) {this.quality = 2;}
+        else {this.quality = 1;}
+    }
+    
+    public final void evolve(){
+        if(!this.healthStat) {
+            this.healthStat = true;
+            this.makeQuality();
+        } else if(!this.attackStat) {
+            this.attackStat = true;
+            this.makeQuality();
+        } else if(!this.speedStat) {
+            this.speedStat = true;
+            this.makeQuality();
+        } else if(!this.skillStat) {
+            this.skillStat = true;
+            this.makeQuality();
+        } else {
+            this.makeQuality();
+        }
+    }
+    
+    public int getQuality() {
+        return quality;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSaga() {
+        return saga;
+    }
+
+    public void setSaga(String saga) {
+        this.saga = saga;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWaitCounter() {
+        return waitCounter;
+    }
+
+    public void setWaitCounter(int waitCounter) {
+        this.waitCounter = waitCounter;
+    }
+
+    public boolean isHealthStat() {
+        return healthStat;
+    }
+
+    public void setHealthStat(boolean healthStat) {
+        this.healthStat = healthStat;
+    }
+
+    public boolean isAttackStat() {
+        return attackStat;
+    }
+
+    public void setAttackStat(boolean attackStat) {
+        this.attackStat = attackStat;
+    }
+
+    public boolean isSpeedStat() {
+        return speedStat;
+    }
+
+    public void setSpeedStat(boolean speedStat) {
+        this.speedStat = speedStat;
+    }
+
+    public boolean isSkillStat() {
+        return skillStat;
+    }
+
+    public void setSkillStat(boolean skillStat) {
+        this.skillStat = skillStat;
+    }
+    
     
 }
--- ProjectSagaWars/src/projectClasses/Processor.java
+++ ProjectSagaWars/src/projectClasses/Processor.java
@@ -3,11 +3,78 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package projectClasses;
+import projectClasses.Fighter;
 
 /**
  *
  * @author juanmendezl
  */
 public class Processor {
+    private int duration;
+    private int probFight;
+    private int probTie;
+    private int probSkip;
+//    private LinkedList winners;
+
+    public Processor(int duration) {
+        this.duration = duration;
+        this.probFight = 40;
+        this.probTie = 27;
+        this.probSkip = 33;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getProbFight() {
+        return probFight;
+    }
+
+    public void setProbFight(int probFight) {
+        this.probFight = probFight;
+    }
+
+    public int getProbTie() {
+        return probTie;
+    }
+
+    public void setProbTie(int probTie) {
+        this.probTie = probTie;
+    }
+
+    public int getProbSkip() {
+        return probSkip;
+    }
+
+    public void setProbSkip(int probSkip) {
+        this.probSkip = probSkip;
+    }
     
+    public final void determinate(Fighter fighter1, Fighter fighter2){
+        double rand = Math.random() * 100;
+        if (rand >= this.probFight + this.probTie) {
+            this.skip(fighter1, fighter2);
+        } else if (rand <= this.probFight) {
+            this.fight(fighter1, fighter2);
+        } else {
+            this.tie(fighter1, fighter2);
+        }
+    }
+    
+    public final void fight(Fighter fighter1,Fighter fighter2){
+        
+    }
+    
+    public final void skip(Fighter fighter1,Fighter fighter2){
+        
+    }
+    
+    public final void tie(Fighter fighter1,Fighter fighter2){
+        
+    }
 }
